75510c3cde485e0e991016c0fac52df9e24dfd9e
Madalin584 <madalinmadalin223@gmail.com>
2020-01-26 19:51:48 +0200

    Initial commit Warehouse exercise implemented Implemented Factory with hashMap IO Exercise Vehicle exercise implemented Drawings(horizontal, vertical line, square) Deleted some stuff from Shape interface

--- src/list/characters/shapes/Shape.java
+++ src/list/characters/shapes/Shape.java
@@ -1,15 +1,5 @@
 package list.characters.shapes;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public interface Shape {
-    List<String> asterisk = new ArrayList<>();
-
     Shape draw();
-
-    default Shape getAsterisk() {
-        asterisk.forEach(System.out::print);
-        return this;
-    }
 }
--- src/list/characters/shapes/VerticalLineImpl.java
+++ src/list/characters/shapes/VerticalLineImpl.java
@@ -3,7 +3,6 @@ package list.characters.shapes;
 public class VerticalLineImpl implements Shape {
     @Override
     public Shape draw() {
-        asterisk.add("*");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 1; j++) {
                 System.out.print("*");
